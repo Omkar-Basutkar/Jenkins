dfe5e0c1fb190e1b3c88c6780b50e05a91cbcca2
omkar <omkar.basutkar@outlook.com>
2023-08-19 20:41:18 +0530

    testing

--- sample.java
+++ sample.java
@@ -1,5 +1,5 @@
 class Simple{  
     public static void main(String args[]){  
-     System.out.println("Hello java from jenkins");  
+     System.out.println("Hello from java & jenkins");  
     }  
 }  
